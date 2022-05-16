c3e25509560e380587a4fffcd682aad330729249
Maykon Taroco <maykontaroco@gmail.com>
2022-05-16 00:04:09 -0300

    Implemented service query and finished service module

--- src/app/servico-prestado.service.ts
+++ src/app/servico-prestado.service.ts
@@ -1,19 +1,32 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ServicoPrestado } from './servico-prestado/servico-prestado-form/servico-prestado';
 import { environment } from '../environments/environment'
+import { ServicoPrestadoBusca } from './servico-prestado/servico-prestado-lista/servico-prestado-busca';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicoPrestadoService {
 
-  apiURL = environment.apiURLBase + '/api/servicos-prestados';
+  apiURL = environment.apiURLBase + 'api/servicos-prestados';
 
   constructor(private http: HttpClient) { }
 
   salvar(servicoPrestado: ServicoPrestado) : Observable<ServicoPrestado> {
     return this.http.post<ServicoPrestado>(this.apiURL, servicoPrestado);
+  }
+
+  buscar(nome: string, mes: number) : Observable<ServicoPrestadoBusca[]>{
+    
+    const httpParams = new HttpParams()
+      .set("nome", nome ? nome : "")
+      .set("mes", mes ? mes.toString() : "");
+
+    const url = this.apiURL + "?" + httpParams.toString();   
+
+    console.log("url => "+ url)
+    return this.http.get<any>(url);
   }
 }
---
+++ src/app/servico-prestado/servico-prestado-lista/servico-prestado-busca.ts
@@ -0,0 +1,8 @@
+import { Cliente } from "../../clientes/cliente";
+
+export class ServicoPrestadoBusca {
+    descricao: string = '';
+    valor: number = 0;
+    data: string = '';
+    cliente: Cliente = new Cliente;
+}
